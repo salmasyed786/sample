7a18f2ffdba196a95b162eaf73c4e73dc4a4ba3e
salmasyed786 <salmasyed7862@gmail.com>
2025-05-22 12:30:55 +0300

    Guess the number and win a game.!

---
+++ guessthenumber.py
@@ -0,0 +1,7 @@
+import random
+a = random.randint(1,100)
+b = int(input("Enter your guess: "))
+if a==b:
+    print("Yay......!! You got it")
+else:
+    print("Aww...........!! better luck next time")
